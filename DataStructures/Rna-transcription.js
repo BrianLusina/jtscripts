80e788c1df6fbf5bd8310a5ce8b05c0445a0bed0
Brian Lusina <lusinabrian@gmail.com>
2016-07-30 22:20:05 +0300

    Add RNA to protein solution

--- DataStructures/Rna-transcription.js
+++ DataStructures/Rna-transcription.js
@@ -1,3 +1,34 @@
+var AMINO_DICT = {
+    'UUC': 'F', 'UUU': 'F',
+    'UUA': 'L', 'UUG': 'L', 'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I',
+    'AUG': 'M',
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S', 'AGU': 'S', 'AGC': 'S',
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'UAU': 'Y', 'UAC': 'Y',
+    'CAU': 'H', 'CAC': 'H',
+    'CAA': 'Q', 'CAG': 'Q',
+    'AAU': 'N', 'AAC': 'N',
+    'AAA': 'K', 'AAG': 'K',
+    'GAU': 'D', 'GAC': 'D',
+    'GAA': 'E', 'GAG': 'E',
+    'UGU': 'C', 'UGC': 'C',
+    'UGG': 'W',
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'AGA': 'R', 'AGG': 'R',
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+};
+    //'UAA': 'Stop', 'UGA': 'Stop', 'UAG': 'Stop'
 function protein(rna) {
-  // your code here
+  var prot_result = "";
+  var codons = rna.split(/([A-Z]{3})/);
+  for(var x = 0;x < codons.length; x++){
+    if(AMINO_DICT.hasOwnProperty(codons[x])){
+      var h = codons[x];
+      prot_result += AMINO_DICT[h];
+    }
+  }
+ return prot_result;
 }
