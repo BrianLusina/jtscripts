78723e4bc2a599fab0555edacd1bbad7a807f399
Brian Lusina <lusinabrian@gmail.com>
2016-08-01 06:35:29 +0300

    Add solution

--- StringManipulation/chromosomeCheck.js
+++ StringManipulation/chromosomeCheck.js
@@ -3,6 +3,6 @@ var ChromosomeCheck = function(chromosome){
 }
 
 ChromosomeCheck.prototype.chromosomeCheck(sperm) {
-
+  return (sperm === "XX") ? "Congratulations! You're going to have a daughter.": "Congratulations! You're going to have a son."
 }
 module.exports = ChromosomeCheck
