34788bcb251f702080c4f315fac59d2f795557bf
Brian Lusina <lusinabrian@gmail.com>
2016-08-03 09:25:18 +0300

    Add name that mimber object and array

--- StringManipulation/NameThatNumber/namethatnumber.spec.js
+++ StringManipulation/NameThatNumber/namethatnumber.spec.js
@@ -2,23 +2,28 @@ var NameNumber = require('./namethatnumber')
 
 describe("Name That Number tests",function(){
   it("test 1, Zero", function(){
-    var name = NameNumber();
+    var name = new NameNumber();
     expect(name.nameThatNumber(0)).toEqual('zero');
   });
 
   it("test 2, Four", function(){
-    var name = NameNumber();
+    var name = new NameNumber();
     expect(name.nameThatNumber(4)).toEqual('four');
   });
 
   it("test 3, Nine", function(){
-    var name = NameNumber();
+    var name = new NameNumber();
     expect(name.nameThatNumber(9)).toEqual('nine');
   });
 
   it("test 23", function(){
-    var name = NameNumber();
+    var name = new NameNumber();
     expect(name.nameThatNumber(23)).toEqual('twenty three');
+  });
+
+  if("test 40", function(){
+    var name = new NameNumber();
+    expect(name.nameThatNumber(40)).toEqual('forty');    
   });
 
 });
