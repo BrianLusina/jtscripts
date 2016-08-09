0f5f1c06c85d5f01019998f7304f3f5d8b3b1158
Brian Lusina <lusinabrian@gmail.com>
2016-08-09 05:58:51 +0300

    Add HexToDec function

--- BinaryAndHex/HexToDec/hex-to-dec.js
+++ BinaryAndHex/HexToDec/hex-to-dec.js
@@ -1,6 +1,6 @@
 var HexToDec = function(){};
 
-HexToDec.prototype.hexToDec = function () {
-
+HexToDec.prototype.hexToDec = function (hexString) {
+  return parseInt(hexString, 16);
 };
 module.exports = HexToDec
