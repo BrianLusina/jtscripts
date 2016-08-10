63fa00ffa42a69c44b26ac3eebd66b353f25767b
Brian Lusina <lusinabrian@gmail.com>
2016-08-10 06:45:17 +0300

    Add Pernicious Test

---
+++ Math/Pernicious/pernicious.spec.js
@@ -0,0 +1,8 @@
+var Pernicious = require('./pernicious')
+
+describe("Pernicious Test suite", function(){
+  it("Test 1", function(){
+    pern = Pernicious();
+    expect(pern.is_pernicious()).toEqual()
+  });
+});
