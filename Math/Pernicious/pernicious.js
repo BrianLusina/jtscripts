f850725730510bade51bd44b13a21c2da2793bc1
Brian Lusina <lusinabrian@gmail.com>
2016-08-10 08:27:09 +0300

    Add isPrime function to Pernicious

--- Math/Pernicious/pernicious.js
+++ Math/Pernicious/pernicious.js
@@ -1,12 +1,26 @@
 var Pernicious = function(){}
 
 Pernicious.prototype.is_pernicious = function (num) {
-  num.toString(10)
+  var b = num.toString(10), prim = [];
+  for(var i = 0; i < b.length; i++){
+    if(this.isPrime(b[i])){
+      prim.push(b[i])
+    }
+  }
 };
 
 
-Pernicious.prototype.isPrime = function(number){
-  
+Pernicious.prototype.isPrime = function(n){
+      if (n < 2) return false;
+
+      var q = parseInt(Math.sqrt(n));
+
+      for (var i = 2; i <= q; i++){
+          if (n % i == 0){
+              return false;
+          }
+      }
+      return true;
 }
 
 module.exports = Pernicious
