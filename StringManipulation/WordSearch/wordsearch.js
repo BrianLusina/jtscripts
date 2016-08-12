cd3b320e44aa35401be73706f7b5f2d891f4ad09
Brian Lusina <lusinabrian@gmail.com>
2016-08-12 07:33:16 +0300

    Edit word Search solution

--- StringManipulation/WordSearch/wordsearch.js
+++ StringManipulation/WordSearch/wordsearch.js
@@ -2,15 +2,13 @@ var WordSearch = function(){};
 
 /*Loop through the seq, splitting each word, and checking if the letters are in the query*/
 WordSearch.prototype.wordSearch = function(query, seq){
-  var results = [], query= query.toLowerCase();
-  for(var x = 0;x < seq.length; x++){
-    for(var y = 0; y < seq[x].length; y++){
-      var c = seq[x][y].toLowerCase();
-      if (query.indexOf(c) != -1){
-        results.push(seq[x]);
-      }
+  var results = [], query = query.toLowerCase();
+  for(var x = 0; x < seq.length; x++){
+    var c = seq[x].toLowerCase();
+    if(c.indexOf(query) != -1){
+      results.push(seq[x]);
+    }
     };
-  };
   return (results.length != 0) ? results : ["Empty"];
 };
 
