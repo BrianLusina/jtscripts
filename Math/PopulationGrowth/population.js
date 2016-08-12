7767888d0f72ab070a960ff5962ddb6bbfc854fd
Brian Lusina <lusinabrian@gmail.com>
2016-08-12 05:53:40 +0300

    Add Population Growth solution

--- Math/PopulationGrowth/population.js
+++ Math/PopulationGrowth/population.js
@@ -1,7 +1,12 @@
 var PopulationGrowth = function(){};
 
 PopulationGrowth.prototype.nbYear = function(p0, percent, aug, p){
-
+  var n = 0;
+  while(p0 < p){
+    p0 += p0 * (percent/100) + aug;
+    n++;
+  }
+  return n;
 };
 
 module.exports = PopulationGrowth
