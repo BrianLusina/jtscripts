f56e97f88db9ffd5b4f5b1497d66611f42f148c7
Brian Lusina <lusinabrian@gmail.com>
2016-08-13 12:32:25 +0300

    Add TitleCase tests

---
+++ StringManipulation/TitleCase/titlecase.spec.js
@@ -0,0 +1,19 @@
+var TitleCase = require('./titlecase')
+
+describe("Title Case Test suite", function(){
+  it("Test 1", function(){
+    var title = new TitleCase();
+    expect(title.titleCase('a clash of KINGS', 'a an the of')).toEqual('A Clash of Kings');
+  });
+
+  it("Test 2", function(){
+    var title = new TitleCase();
+    expect(title.titleCase('THE WIND IN THE WILLOWS', 'The In')).toEqual('The Wind in the Willows');
+  });
+
+  it("Test 3", function(){
+    var title = new TitleCase();
+    expect(title.titleCase('the quick brown fox')).toEqual('The Quick Brown Fox');
+  });
+
+});
