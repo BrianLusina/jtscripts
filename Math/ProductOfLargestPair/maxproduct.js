0302cd746c2e9cbf4e62e7d47266170a8c05fcc4
Brian Lusina <lusinabrian@gmail.com>
2016-08-15 06:30:39 +0300

    Add Product Of Largest Pair Solution

--- Math/ProductOfLargestPair/maxproduct.js
+++ Math/ProductOfLargestPair/maxproduct.js
@@ -1,6 +1,18 @@
 var MaxProduct = function(){};
 
-MaxProduct.prototype.maxProduct = function(array){
+MaxProduct.prototype.maxProduct = function(arr){
+  var biggest = -Infinity, next_biggest = -Infinity;
+  var max = Math.max.apply(null, arr);
+  for (var i = 0, n = arr.length; i < n; ++i) {
+      var nr = +arr[i]; // convert to number first
+      if (nr > biggest) {
+          next_biggest = biggest; // save previous biggest value
+          biggest = nr;
+      } else if (nr < biggest && nr > next_biggest) {
+          next_biggest = nr; // new second biggest value
+      }
+  }
+  return max * next_biggest;
 
 };
 
--- Math/ProductOfLargestPair/maxproduct.spec.js
+++ Math/ProductOfLargestPair/maxproduct.spec.js
@@ -1,54 +1,49 @@
 var MaxProduct = require('./maxproduct')
 
 describe("Max Product Test Suite", function(){
-  it("Test 1", function(){
-    var m = new MaxProduct();
-    expect(m.maxProduct()).toEqual();
-  });
-
   it("Test 2", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([56, 335, 195, 443, 6, 494, 252]).toEqual(218842);
+    expect(m.maxProduct([56, 335, 195, 443, 6, 494, 252])).toEqual(218842);
   });
 
   it("Test 3", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([154, 428, 455, 346]).toEqual(194740);
+    expect(m.maxProduct([154, 428, 455, 346])).toEqual(194740);
   });
 
   it("Test 4", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([39, 135, 47, 275, 37, 108, 265, 457, 2, 133, 316, 330, 153, 253, 321, 411]).toEqual(187827);
+    expect(m.maxProduct([39, 135, 47, 275, 37, 108, 265, 457, 2, 133, 316, 330, 153, 253, 321, 411])).toEqual(187827);
   });
 
   it("Test 5", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([136, 376, 10, 146, 105, 63, 234]).toEqual(87984);
+    expect(m.maxProduct([136, 376, 10, 146, 105, 63, 234])).toEqual(87984);
   });
 
   it("Test 6", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([354, 463, 165, 62, 472, 53, 347, 293, 252, 378, 420, 398, 255, 89]).toEqual(218536);
+    expect(m.maxProduct([354, 463, 165, 62, 472, 53, 347, 293, 252, 378, 420, 398, 255, 89])).toEqual(218536);
   });
 
   it("Test 7", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([346, 446, 26, 425, 432, 349, 123, 269, 285, 93, 75, 14]).toEqual(192672);
+    expect(m.maxProduct([346, 446, 26, 425, 432, 349, 123, 269, 285, 93, 75, 14])).toEqual(192672);
   });
 
   it("Test 8", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([134, 320, 266, 299]).toEqual(95680);
+    expect(m.maxProduct([134, 320, 266, 299])).toEqual(95680);
   });
 
   it("Test 9", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([114, 424, 53, 272, 128, 215, 25, 329, 272, 313, 100, 24, 252]).toEqual(139496);
-  }):
+    expect(m.maxProduct([114, 424, 53, 272, 128, 215, 25, 329, 272, 313, 100, 24, 252])).toEqual(139496);
+  });
 
   it("Test 10", function(){
     var m = new MaxProduct();
-    expect(m.maxProduct([375, 56, 337, 466, 203]).toEqual(174750);
+    expect(m.maxProduct([375, 56, 337, 466, 203])).toEqual(174750);
   });
-  
+
 });
