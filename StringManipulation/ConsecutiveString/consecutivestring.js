b55549be52d4767a5ea1dd21f5f3cb55ed26e8e7
Brian Lusina <lusinabrian@gmail.com>
2016-08-17 06:31:41 +0300

    Add conditionals
    Check through k and starr length to return empty string if k <== 0 or length of starr is 0
    sorts array and returns the longest string if k == 1

--- StringManipulation/ConsecutiveString/consecutivestring.js
+++ StringManipulation/ConsecutiveString/consecutivestring.js
@@ -1,7 +1,20 @@
 var ConsecutiveString = function(){};
 
 ConsecutiveString.prototype.longestConsec = function(starr, k){
+  var out = ""
+  if((k <= 0 || starr.length == 0) || k > starr.length){
+    return out;
+  }
+  else if(k === 1){
+    var s = starr.sort(function(b, a){
+      return a - b;
+    });
+    out = s[s.length-1]
+  }else{
+    
+  }
 
-}
+  return out;
+};
 
 module.exports = ConsecutiveString
--- StringManipulation/ConsecutiveString/consecutivestring.spec.js
+++ StringManipulation/ConsecutiveString/consecutivestring.spec.js
@@ -13,37 +13,37 @@ describe("Consecutive String Test Cases", function(){
 
   it("Test 3", function(){
     var longCon = new ConsecutiveString();
-    expect(longCon.longestConsec([], 3).toEqual("");
+    expect(longCon.longestConsec([], 3)).toEqual("");
   });
 
   it("Test 4", function(){
     var longCon = new ConsecutiveString();
-    expect(longCon.longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2).toEqual("wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+    expect(longCon.longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2)).toEqual("wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
   });
 
   it("Test 5", function(){
     var longCon = new ConsecutiveString();
-    expect(longCon.longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2).toEqual("wlwsasphmxxowiaxujylentrklctozmymu");
+    expect(longCon.longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2)).toEqual("wlwsasphmxxowiaxujylentrklctozmymu");
   });
 
   it("Test 6", function(){
       var longCon = new ConsecutiveString();
-      expect(longCon.longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2).toEqual("");
+      expect(longCon.longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2)).toEqual("");
   });
 
   it("Test 7", function(){
       var longCon = new ConsecutiveString();
-      expect(longCon.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3).toEqual("ixoyx3452zzzzzzzzzzzz");
+      expect(longCon.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3)).toEqual("ixoyx3452zzzzzzzzzzzz");
   });
 
   it("Test 8", function(){
       var longCon = new ConsecutiveString();
-      expect(longCon.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15).toEqual("");
+      expect(longCon.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15)).toEqual("");
   });
 
   it("Test 9", function(){
       var longCon = new ConsecutiveString();
-      expect(longCon.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0).toEqual("");
+      expect(longCon.longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0)).toEqual("");
   });
 
 });
