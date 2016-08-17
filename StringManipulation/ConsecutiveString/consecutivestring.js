f080a5d599e880e8a618465d17924675ad37c85b
Brian Lusina <lusinabrian@gmail.com>
2016-08-17 06:52:12 +0300

    Add loop
    Loop through each string in array concateneting them based on the number k.

--- StringManipulation/ConsecutiveString/consecutivestring.js
+++ StringManipulation/ConsecutiveString/consecutivestring.js
@@ -1,7 +1,11 @@
 var ConsecutiveString = function(){};
 
+/*validate k and length of the array. If the array length is 0 of less than k of k is less thatn or equal to 0
+return an empty string.
+if k is 1, then return the longest string.
+else, loop through the string, */
 ConsecutiveString.prototype.longestConsec = function(starr, k){
-  var out = ""
+  var out = "", concat = "";
   if((k <= 0 || starr.length == 0) || k > starr.length){
     return out;
   }
@@ -11,7 +15,13 @@ ConsecutiveString.prototype.longestConsec = function(starr, k){
     });
     out = s[s.length-1]
   }else{
-    
+    for(var x = 0; x <= k; x++){
+      var f3 = starr[x] + starr[x+1] + starr[x+2];
+      var n3 = starr[x+1] + starr[x+2] + starr[x+3];
+      if(n3.length > f3.length){
+        starr[x] = null;
+      }
+    }
   }
 
   return out;
