3e9126632659dccfd06055d24b13110969b047cd
Brian Lusina <lusinabrian@gmail.com>
2016-08-22 17:36:56 +0300

    Add get short messages solution

--- Arrays/getShortMessages.js
+++ Arrays/getShortMessages.js
@@ -1,5 +1,9 @@
 function getShortMessages(messages) {
-  // SOLUTION GOES HERE
+  return messages.filter(function(obj){
+    return obj['message'].length < 50;
+  }).map(function(v){
+    return v['message'];
+  });
 }
 
 module.exports = getShortMessages
