0ed43edd2eabb8edda94fe6f91f271ed00d7096b
Brian Lusina <lusinabrian@gmail.com>
2016-08-22 17:17:22 +0300

    Edit higher order function solution

--- HigherOrderFunctions/repeat.js
+++ HigherOrderFunctions/repeat.js
@@ -1,8 +1,8 @@
 function repeat(operation, num) {
-  if(num == 1 || num == 0){
+  if(num <= 0){
     return operation();
   }
-  return repeat(operation(), num-1);
+  return repeat(operation, --num);
 }
 
 // Do not remove the line below
