75631ab05434e9561a9c718bd080612b74b43fad
Brian Lusina <lusinabrian@gmail.com>
2016-08-24 07:06:37 +0300

    Add Prototype and Objects description

--- Recursion/reducer.js
+++ Recursion/reducer.js
@@ -1,5 +1,14 @@
 function reduce(arr, fn, initial) {
-    // SOLUTION GOES HERE
+  if(!arr.length)
+    return {};
+
+  var head = arr[0];
+  var tail = arr.slice(1);
+
+  return reduce(arr, fn(prev, curr, indx, arr){
+      return prev + curr;
+  }, initial){
+  }
   }
 
 module.exports = reduce
