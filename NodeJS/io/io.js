2ac7632add968e80e40e0c0951caa797ad999b05
Brian Lusina <lusinabrian@gmail.com>
2016-08-27 08:50:40 +0300

    Add io file system solution

---
+++ NodeJS/io/io.js
@@ -0,0 +1,23 @@
+// get the fs module from Node Core library
+var fs = require('fs');
+
+// store the file contents in a var buffer and convert to string
+var buffer = fs.readFileSync(process.argv[2]), contents = buffer.toString();
+
+var c = contents.split("\n").length-1;
+
+console.log(c)
+
+// Alternatively
+/**
+var fs = require('fs')
+
+    var contents = fs.readFileSync(process.argv[2])
+    var lines = contents.toString().split('\n').length - 1
+    console.log(lines)
+
+    // note you can avoid the .toString() by passing 'utf8' as the
+    // second argument to readFileSync, then you'll get a String!
+    //
+    // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1  
+*/
