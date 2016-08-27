13c550dbbf98816b96422249d330bf4fd30f5a58
Brian Lusina <lusinabrian@gmail.com>
2016-08-27 09:54:09 +0300

    Update DuckCount solution

--- Objects/duckCount.js
+++ Objects/duckCount.js
@@ -1,5 +1,5 @@
 function duckCount() {
-  return Array.protoype.slice.call(arguments).filter((obj) => {
+  return Array.prototype.slice.call(arguments).filter((obj) => {
     return Object.prototype.hasOwnProperty.call(obj, 'quack')
   }).length;
 }
