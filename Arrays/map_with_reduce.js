825d17f72334eb37a3b2b094b13a6ea221b931ff
Brian Lusina <lusinabrian@gmail.com>
2016-08-30 22:59:09 +0300

    Update map with reduce

--- Arrays/map_with_reduce.js
+++ Arrays/map_with_reduce.js
@@ -1,6 +1,6 @@
 module.exports = function arrayMap(arr, fn, thisArg) {
-    return arr.reduce(function(a, item, indx, arr){
-        a.push(fn.call(thisArg, item, indx, arr));
-        return a;        
+    return arr.reduce(function(prev, curr, curr_indx, arr){
+        prev.push(fn.call(thisArg, curr, curr_indx, arr));
+        return prev;        
     },[]);
 }
