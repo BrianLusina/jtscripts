47611ade1c9edc639c3f3df09ea7a9ee8a4df502
Brian Lusina <lusinabrian@gmail.com>
2016-08-31 06:22:57 +0300

    Update node server

--- NodeJS/Networks/hello_http.js
+++ NodeJS/Networks/hello_http.js
@@ -3,5 +3,10 @@ var http = require('http');
 http.createServer((request, response) => {
     response.writeHead(200); //status code in header
     response.write("Hello world"); //write body
-    response.end(); //end connection
-});
+    setTimeout(function(){
+        response.write("Long running process");
+        response.end(); //end connection
+    }, 5000);
+}).listen(8080);
+
+console.log("Listening on port 8080");
