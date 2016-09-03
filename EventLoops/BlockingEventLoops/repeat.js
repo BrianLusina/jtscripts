f8058a92c5902f2e1680b006bc90004959a7b0de
Brian Lusina <lusinabrian@gmail.com>
2016-09-03 11:38:37 +0300

    Add Blocking events solutions

---
+++ EventLoops/BlockingEventLoops/repeat.js
@@ -0,0 +1,24 @@
+function repeat(operation, num, callback) {
+    setTimeout(function(){
+        if (num <= 0) return callback();
+        operation();
+        return repeat(operation, --num, callback)
+    },0);
+}
+
+// altenative solution
+function repeat_2(operation, num){
+    if(num <= 0) return;
+    operation();
+
+    //release operation every 10 iterations
+    if(num % 10 === 0){
+        setTimeout(function(){
+            repeat_2(operation, --num);
+        });
+    }else{
+        repeat_2(operation, --num);
+    }
+}
+
+module.exports = repeat
