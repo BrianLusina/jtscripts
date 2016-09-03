aa01e86d5d73f8e6cec3edc6c41374adab0a5e01
Brian Lusina <lusinabrian@gmail.com>
2016-09-03 11:26:25 +0300

    Add Function spies solution

--- functions/Functions_Spies/spy.js
+++ functions/Functions_Spies/spy.js
@@ -1,5 +1,13 @@
 function Spy(target, method){
+    var spy = {count:0};
+    var oldie = target[method];
     
+    target[method] = function(){
+        spy.count++;
+        return oldie.apply(target, arguments);
+    } 
+
+    return spy;
 }
 
 module.exports = Spy
