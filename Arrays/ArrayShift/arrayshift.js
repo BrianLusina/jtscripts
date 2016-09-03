e31650fd3503c3ad407c5d1320a9b5edfc5afa8d
Brian Lusina <lusinabrian@gmail.com>
2016-09-03 10:53:11 +0300

    add addedShifted solutino

---
+++ Arrays/ArrayShift/arrayshift.js
@@ -0,0 +1,16 @@
+var ArrayShift = function(){};
+
+ArrayShift.prototype.addingShifted = function(arrayOfArrays, shift) {
+    var result = [], endArrayLength = arrayOfArrays[0].length + shift * (arrayOfArrays.length - 1);
+    for(var i = 0;i < endArrayLength;i++){
+      result.push(0);
+    }
+    for(var x=0; x < arrayOfArrays.length; x++){
+      for(var j=0;j < arrayOfArrays[x].length; j++){
+        result[j + shift * x] += arrayOfArrays[x][j];
+      }
+    } 
+    return result;
+}
+
+module.exports = ArrayShift
--- Arrays/ArrayShift/arrayshift.spec.js
+++ Arrays/ArrayShift/arrayshift.spec.js
@@ -5,21 +5,21 @@ describe("Array Shift test suite", function(){
         var arrayOfArrays = [[1, 2, 3, 4, 5, 6], [7, 7, 7, 7, 7, -7]];
         var expected = [8, 9, 10, 11, 12, -1 ];
         var arr = new ArrayShift();
-        expect(arr.addedShifted(arrayOfArrays, 0)).toEqual(expected);
+        expect(arr.addingShifted(arrayOfArrays, 0)).toEqual(expected);
     });
    
     it("Test two", function(){
         var arrayOfArrays = [[1, 2, 3, 4, 5, 6],[7, 7, 7, 7, 7, 7]];
         var expected = [ 1, 2, 3, 11, 12, 13, 7, 7, 7];
         var arr = new ArrayShift();
-        expect(arr.addedShifted(arrayOfArrays, 3)).toEqual(expected);
+        expect(arr.addingShifted(arrayOfArrays, 3)).toEqual(expected);
     });
     
     it("Test three", function(){
         var arrayOfArrays = [[1, 2, 3, 4, 5, 6], [7, 7, 7, -7, 7, 7],[1, 1, 1, 1, 1, 1]];
         var expected = [1, 2, 3, 11, 12, 13, -6, 8, 8, 1, 1, 1];
         var arr = new ArrayShift();
-        expect(arr.addedShifted(arrayOfArrays, 3)).toEqual(expected);
+        expect(arr.addingShifted(arrayOfArrays, 3)).toEqual(expected);
     });
 
 });
