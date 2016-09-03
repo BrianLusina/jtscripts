68f163c4b953aa2bd05795d681d6cd422e622f4f
Brian Lusina <lusinabrian@gmail.com>
2016-09-03 12:35:33 +0300

    Add Curry functions

---
+++ functions/Curry/curry.js
@@ -0,0 +1,8 @@
+function curryN(fn, n) {
+    n = n || fn.length;
+    return function curryMe(arg){
+        return (n <= 1) ? fn(arg) : curryN(fn.bind(this,arg), n-1);
+    }
+}
+
+module.exports = curryN
