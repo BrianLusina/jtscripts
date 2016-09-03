b378e78ef502d67cad75e057462abc7d8b73d9b8
Brian Lusina <lusinabrian@gmail.com>
2016-09-03 12:00:45 +0300

    Add async loops

---
+++ EventLoops/AsyncLoops/async.js
@@ -0,0 +1,13 @@
+function loadUsers(userIds, load, done) {
+    var finito = 0, users = [];
+    userIds.forEach(function(id, indx){
+        load(id, function(user){
+            users[indx] = user;
+            if(++finito === userIds.length){
+                return done(users);
+            }
+        });
+    });
+}
+
+module.exports = loadUsers
