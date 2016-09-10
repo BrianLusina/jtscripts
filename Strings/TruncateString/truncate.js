75c8d3fdf1f54fc2d25aa3186439427736c2fb6c
Brian Lusina <lusinabrian@gmail.com>
2016-09-10 21:13:26 +0300

    Update Say problem

---
+++ Strings/TruncateString/truncate.js
@@ -0,0 +1,16 @@
+function truncateString(str, num) {
+  var dots ="...";
+  
+  if(num<dots.length){
+    return str.slice(0,num) + dots;
+  }
+  if(str.length>num){
+    return str.slice(0,num-dots.length) + dots;
+  }else{
+    return str;
+  }
+  
+}
+
+console.log(truncatetring("pippi", 3));
+
