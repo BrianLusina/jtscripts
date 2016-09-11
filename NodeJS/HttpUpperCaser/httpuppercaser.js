5faaf900b75fe994e0b61e77bc10ed5a6ffe48a2
Brian Lusina <lusinabrian@gmail.com>
2016-09-11 09:58:27 +0300

    Add HTTP uppercaser logic

---
+++ NodeJS/HttpUpperCaser/httpuppercaser.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var map = require('through2-map');
+
+var server = http.createServer((request, response) => {
+    if(request.method != "POST"){
+        return response.end("Send me a post\n");
+    }
+    request.pipe(map((chunk) => {
+        return chunk.toString().toUpperCase();
+    })).pipe(response);
+})
+
+server.listen(+process.argv[2])
