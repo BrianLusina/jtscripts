a52733b4a7396ac1f8da75abd3ce0a49e4f89f8a
Brian Lusina <lusinabrian@gmail.com>
2016-09-11 10:11:41 +0300

    Add HTTP json api logic

---
+++ NodeJS/HttpJsonApi/httpjsonapi.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var url = require('url');
+
+// return a JSON object with the hour minute and second
+function parseTime(time){
+    return {
+        hour: time.getHours,
+        minute: time.getMinutes(),
+        second: time.getSeconds(),
+    }
+}
+
+// returns a JSON response with time in unix
+function unixTime(time){
+    return {unixtime: time.getTime()};
+}
+
+var server = http.createServer((request, response) => {
+    var parseUrl = url.parse(request.url, true);
+    var time = new Date(parseUrl.query.iso);
+    var result;
+
+    if (/^\/api\/parsetime/.test(request.url))
+        result = parseTime(time);
+    else if (/^\/api\/unixtime/.test(request.url))
+        result = unixTime(time);
+    
+    if (result) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(result));
+    } else {
+        response.writeHead(404);
+        response.end();
+    }
+});
+
+
+server.listen(+process.argv(2));
