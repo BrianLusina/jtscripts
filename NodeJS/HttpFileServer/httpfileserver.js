0173c9f237d7e6e3b378b3c5eccc5468a3fb5bad
Brian Lusina <lusinabrian@gmail.com>
2016-09-11 09:38:29 +0300

    Edit reponse from httpserver

--- NodeJS/HttpFileServer/httpfileserver.js
+++ NodeJS/HttpFileServer/httpfileserver.js
@@ -4,7 +4,7 @@ var http = require('http');
 // create the server
 var server = http.createServer((request, response) => {
     response.writeHead(200, {"content-type" : "text/plain"});
-    fs.createReadStream(process.argv[3].pipe(response));
+    fs.createReadStream(process.argv[3]).pipe(response);
 });
 
 // listen on the port provided
