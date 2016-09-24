80c35fea58b686d763ad17518b68f224c0bc5078
BrianLusina <lusinabrian@gmail.com>
2016-09-24 18:27:25 +0300

    Add Aggegate with MongoDB

---
+++ MongoD/Aggregate.js
@@ -0,0 +1,27 @@
+/**
+ * Created by lusinabrian on 24/09/16.
+ */
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/learnyoumongo";
+var size = process.argv[2];
+
+mongo.connect(url, (err, db)=>{
+    if(err) throw err;
+    var collection = db.collection("prices");
+    var match = {$match:{size : size}};
+    var group = { $group: {
+        _id: 'average'
+            , average: {
+            $avg: '$price'
+        }
+    }};
+    collection.aggregate([match,group]).toArray(function(err, results){
+        if(err) throw err;
+        if(!results.length){
+            throw new Error("No results were found");
+        }
+        var m = results[0];
+        console.log(Number(m.average).toFixed(2));
+        db.close();
+    });
+});
