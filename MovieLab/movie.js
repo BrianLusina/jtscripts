fe08be1954f46150b99433379c2f11d8976f57ed
BrianLusina <lusinabrian@gmail.com>
2016-11-19 12:49:48 +0300

    add movie labe

--- MovieLab/movie.js
+++ MovieLab/movie.js
@@ -1,11 +1,11 @@
 var canIWatch = function(age){
-    if(age < 6){
+    if(age > 0 && age < 6){
         return "You are not allowed to watch Deadpool after 6.00pm.";
     }else if(age >= 6 && age < 17){
         return "You must be accompanied by a guardian who is 21 or older.";
     }else if(age >= 17 && age < 25){
         return "You are allowed to watch Deadpool, right after you show some ID.";
-    }else if(age > 25){
+    }else if(age >= 25){
         return "Yay! You can watch Deadpool with no strings attached!";
     }else{
         return "Invalid age.";
