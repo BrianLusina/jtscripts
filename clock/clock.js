73d31a009b0d8c5ff19266e16eac1f628581f007
BrianLusina <lusinabrian@gmail.com>
2016-11-24 19:54:11 +0300

    Add Clock solution

--- clock/clock.js
+++ clock/clock.js
@@ -1,3 +1,5 @@
+module.at = at;
+
 const HOURS_IN_A_DAY = 24;
 const MINUTES_IN_AN_HOUR = 60;
 const MINUTES_IN_A_DAY = HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR;
@@ -5,16 +7,45 @@ const MILLIS_IN_A_MINUTE = 60 * 1000;
 const MILLIS_IN_AN_HOUR = MINUTES_IN_AN_HOUR * MILLIS_IN_A_MINUTE;
 const MILLIS_IN_A_DAY = HOURS_IN_A_DAY * MILLIS_IN_AN_HOUR;
 
-
-var Clock = function(hour, minutes){
-    minutes = minutes || 0;
-    
-    this.hour = hour;
-    this.minutes = minutes;
-
-
+function convertPos(time, maxVal){
+    time %= maxVal;
+    time += maxVal;
+    return time;
 }
 
+function at(hours, minutes){
+    minutes = minutes || 0;
+    
+    hours = convertPos(hours, HOURS_IN_A_DAY);
+    minutes = convertPos(minutes, MINUTES_IN_A_DAY);
+    
+    var millis = (hours * MILLIS_IN_AN_HOUR) + (minutes * MILLIS_IN_A_MINUTE);
 
+    millis = convertPos(value, MILLIS_IN_A_DAY);
 
-module.exports = Clock;
+    var clock = {};
+    clock.valueOf = function(){
+        return millis;
+    }
+
+    clock.toString = function(){
+        let time = new Date(millis).toISOString().split("T")[1].split(":");
+        return `${time[0]}:${time[1]}`;
+    }
+
+    clock.plus = function(minutes){
+        millis += minutes * MILLIS_IN_A_MINUTE;
+        return clock; 
+    }
+
+    clock.minus = function(minutes){
+        millis -= minutes * MILLIS_IN_A_MINUTE;
+        return clock;
+    }
+
+    clock.equals = function(other){
+        return +clock === +other;
+    }
+
+    return Object.create(clock);
+}
