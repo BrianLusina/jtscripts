308662c6e9f8f468fc7c135b9416c9bfa6ca4505
BrianLusina <lusinabrian@gmail.com>
2016-11-30 22:16:41 +0300

    update fibonacci

--- Math/Fibonacci/Xbonacci/fibTriFriends/fibTriFriends.js
+++ Math/Fibonacci/Xbonacci/fibTriFriends/fibTriFriends.js
@@ -11,9 +11,11 @@ check if the signature is equal to the desired length and stop the loop if so, e
 */
 function Xbonacci(signature,n){
   var next = 0, sigCopy = signature.slice(), first;
-  if(signature.length === 1 || signature.length === 0){
+  if(signature.length === 1 || 0){
     return signature;
-  }else if(signature.length === 2){
+  }
+  
+  if(signature.length === 2){
     for (var x = 0; x < signature.length; x++){
       next = signature[x] + signature[x+1];
       signature.push(next);
@@ -35,7 +37,5 @@ function Xbonacci(signature,n){
   return signature;
 }
 
-Test.assertSimilar(Xbonacci([0,1],10),[0,1,1,2,3,5,8,13,21,34])
-Test.assertSimilar(Xbonacci([1,1],10),[1,1,2,3,5,8,13,21,34,55])
-Test.assertSimilar(Xbonacci([0,0,0,0,1],10),[0,0,0,0,1,1,2,4,8,16])
-Test.assertSimilar(Xbonacci([1,0,0,0,0,0,1],10),[1,0,0,0,0,0,1,2,3,6])
+
+module.exports = Xbonacci;
