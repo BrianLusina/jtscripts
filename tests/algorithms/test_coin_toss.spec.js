920dcace4b7b38084a1095371108b6f44badd587
BrianLusina <lusinabrian@gmail.com>
2017-01-05 21:05:53 +0300

    add coin toss

---
+++ tests/algorithms/test_coin_toss.spec.js
@@ -0,0 +1,12 @@
+var coinToss = require("../../jssnips/algorithms/coin_toss/coin_toss.js");
+
+describe("Test if coin toss", function(){
+    // it("accepts number arguments only", function(){
+    //     expect(function(){coinToss("2")}).toThrow();
+    // });
+
+    it("returns a number", function(){
+        expect(coinToss(2)).toBeTruthy();
+    });
+
+});
