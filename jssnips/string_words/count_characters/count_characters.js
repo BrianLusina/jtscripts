4a6ced04db3227825166db7217e4712ee07cec9c
BrianLusina <lusinabrian@gmail.com>
2017-01-05 20:35:53 +0300

    add count characters tests

---
+++ jssnips/string_words/count_characters/count_characters.js
@@ -0,0 +1,31 @@
+function total_characters(wordArray) {
+    //check if parameter is a valid array/ or is an array
+    if(!wordArray instanceof Array){
+        throw new TypeError(`Invalid parameter $wordArray`);
+    }
+
+    // create a variable that will have initial total set to 0
+    var total = 0;
+
+    //early return, if the array is empty
+    if(wordArray.length == 0){
+        return 0;
+    }
+
+    // loop through array and for each word get the length of the array
+    // add this total to the total variable.
+    // check if the word is a word, if not, add 0 instead
+    for(var x = 0; x < wordArray.length; x++){
+        if(typeof wordArray[x] == "string"){
+            total += wordArray[x].length;
+        }else{
+            total += 0;
+        }
+    }
+
+    // return the total
+    return total;
+}
+
+module.exports = total_characters
+
---
+++ tests/string_words/test_count_characters.spec.js
@@ -0,0 +1,29 @@
+var total_characters = require("../../jssnips/string_words/count_characters/count_characters.js");
+
+describe("Total Characters test", function(){
+    it("Test for words", function(){
+        expect(total_characters([ "adios", "bye", "ciao" ])).toEqual(12);
+    });
+
+    it("Test for array of numbers", function(){
+        expect(total_characters([ 4 , 5, 6 ])).toEqual(0);
+    });
+
+    it("Test for empty array", function(){
+        expect(total_characters([])).toEqual(0);        
+    });
+
+    it("Test for array of length 1", function(){
+        expect(total_characters(["brianlusinaombito"])).toEqual(17);
+    });
+
+    it("Test for different types of variables", function(){
+        expect(total_characters(["Brian", 5, {}, "lusina"])).toEqual(11);
+    });
+
+    it("Test for wrong arguments", function(){
+        expect(function(){
+            total_characters(9);
+        }).toThrow(new TypeError("Invalid parameter 9"));
+    });
+});
