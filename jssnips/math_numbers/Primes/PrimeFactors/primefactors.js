2ff41a220dee7513677b4a2e73bbfb765fd9ce58
BrianLusina <lusinabrian@gmail.com>
2017-01-17 19:46:41 +0300

    add prime factors

--- jssnips/math_numbers/Primes/PrimeFactors/primefactors.js
+++ jssnips/math_numbers/Primes/PrimeFactors/primefactors.js
@@ -1,16 +1,17 @@
 exports.for = function(number){
     var primes = [];
     var candidate = 2;
-
-    while(candidate <= number){
-        // check if the number is divisible
-        if(number % candidate == 0){
+    
+    while(candidate * candidate <= number){
+        while(number % candidate == 0){
             primes.push(candidate);
-            number %= candidate;
+            number /= candidate;
         }
-
         candidate += 1;
     }
-
+    if(number > 1){
+        primes.push(number);
+    }
+    
     return primes;
 }
