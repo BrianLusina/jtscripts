92f1c1d5df51309143dc51ed845ea04ee767a6e7
BrianLusina <lusinabrian@gmail.com>
2017-01-25 19:38:23 +0300

    add strain solution

---
+++ jssnips/algorithms/strain/strain.js
@@ -0,0 +1,42 @@
+'use strict';
+let strain = {
+    create(array, filter, keepMatches){
+        var results = [];
+        for(var x = 0;x< array.length; x++){
+            if(filter(array[x]) === keepMatches){
+                results.push(array[x]);
+            }
+        }
+
+        return results;
+    },
+
+    keep(array, filter){
+        return this.create(array, filter, true);
+    },
+
+    discard(array, filter){
+        return this.create(array, filter, false);
+    }
+}
+
+module.exports = {
+  strain: function(array, filter, keepMatches) {
+    var results = [];
+    for (var i=0; i < array.length; i++) {
+      var item = array[i];
+      if (filter(item) === keepMatches) {
+        results.push(item);
+      }
+    }
+    return results;
+  },
+
+  keep: function (array, filter) {
+    return this.strain(array, filter, true);
+  },
+
+  discard: function (array, filter) {
+    return this.strain(array, filter, false);
+  }
+};
