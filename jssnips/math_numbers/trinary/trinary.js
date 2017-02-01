43b95aa2cc6931720b430717068026acae66b4a4
BrianLusina <lusinabrian@gmail.com>
2017-02-01 21:41:32 +0300

    add trinary solution

--- jssnips/math_numbers/trinary/trinary.js
+++ jssnips/math_numbers/trinary/trinary.js
@@ -5,5 +5,16 @@ class Trinary{
         this.number = number;
     }
 
-    
+    /**Converts given trinary number to decimal
+     * by first principles.
+     */
+    toDecimal(){
+        // split and reverse the array of numbers
+        var list = this.number.split("").reverse();
+        var sum = 0;
+        for(var x = 0;x < list.length; x++){
+            sum+= +list[x] * Math.pow(3, x);
+        }
+        return sum;
+    }
 }
