f4028cd86177ea6438c4c347bfe90042c226d216
BrianLusina <lusinabrian@gmail.com>
2017-02-08 20:53:21 +0300

    Update sieve soln

--- jssnips/math_numbers/sieve/sieve.js
+++ jssnips/math_numbers/sieve/sieve.js
@@ -1,46 +1,31 @@
-class Sieve{
-  constructor(limit){
-    this.limit = limit;
+'use strict';
+
+function newArrayWithRange(first, last) {
+  var i, array = [];
+  for ( i = first; i <= last; i++ ) {
+    array.push(i);
   }
-
-  /**
-  Get all prime numbers from 2 upto the limit
-  */
-  primes(){
-    var primeNum, primes = [];
-    var possibilities = this.createRange(2, this.limit);
-
-    do {
-      primeNum = possibilities.shift();
-      primes.push(primeNum);
-      possibilities = possibilities.filter(this.indivisibleBy, primeNum);
-    } while (possibilities.length > 0);
-
-    return primes;
-  }
-
-  /**
-  Checks if a value is indivisible By the prime number
-  @param {Number} value
-  @return {Boolean}
-  */
-  indivisibleBy(value){
-    return value % this !== 0;
-  }
-
-  /**
-  Creates a range from first number to the limit which is the last
-  @param {Number} first the first number in the sequence/range, which is 2
-  @param {Number} last the last, or limit number in the range
-  @return {Array} an array which will represent the range
-  */
-  createRange(first, last){
-    var arr = [];
-    for(var i = first; i <= last; i++){
-      arr.push(i);
-    }
-    return arr;
-  }
+  return array;
 }
 
-module.exports = Sieve;
+function indivisibleBy(value) {
+  return value % this !== 0;
+}
+
+function sieve(n) {
+  var prime, possibilities, primes = [];
+
+  possibilities = newArrayWithRange(2, n);
+
+  do {
+    prime = possibilities.shift();
+    primes.push(prime);
+    possibilities = possibilities.filter( indivisibleBy, prime );
+  } while (possibilities.length > 0);
+
+  return primes;
+}
+
+module.exports = function (n) {
+  this.primes = sieve(n);
+};
