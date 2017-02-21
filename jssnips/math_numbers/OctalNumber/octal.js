8d9fd67ff18db539184326e690122b464894afa6
BrianLusina <lusinabrian@gmail.com>
2017-02-21 20:10:42 +0300

    add octal function

---
+++ jssnips/math_numbers/OctalNumber/octal.js
@@ -0,0 +1,43 @@
+class Octal{
+    constructor(number){
+        this.number = number;
+    }
+
+    /**
+     * Validates user input and checks if the number input is a valid number
+     * returns False if the number is invalid
+     * @returns {Boolean}
+     */
+    validateInput(){
+        return this.number.match(/[0-9]+/gi);
+    }
+
+    /**Converts a decimal number to Octal
+     * @returns {Number} an octal number
+     */
+    toDecimal(){
+        
+        //check if the number is valid
+        if(!this.validateInput()){
+            return 0;
+        }
+        // split the number string to an array
+        // loop through each number multiplying it by 8 to the power of the current index
+        var numberArr = this.number.split("").reverse();
+        var result = [];
+
+        // if the number 8 is found, return early with 0, 8 is invalid.
+        for(var n = 0;n < numberArr.length; n++){
+            if(numberArr[n] === "8"){
+                return 0;
+            }
+            result.push(numberArr[n] * Math.pow(8, n));
+        }
+        
+        return result.reduce(function(a, b){
+            return a + b;
+        });
+    }
+}
+
+module.exports = Octal;
