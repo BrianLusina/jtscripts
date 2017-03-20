533a871d3c6be4b3831f9bae05741128d18f8bb7
BrianLusina <lusinabrian@gmail.com>
2017-03-20 19:44:46 +0300

    add series test cases

---
+++ jssnips/algorithms/series/series.js
@@ -0,0 +1,17 @@
+class Series{
+    constructor(number_str){
+        this.number_str = number_str;
+        this.digits = getDigits();
+    }
+
+    /**
+     * Gets the digits from the provided number and returns an array of numbers
+     * @returns {Array}
+     */
+    getDigits(){
+
+        return [];
+    }
+}
+
+module.exports = Series;
