4a0caf0ead54b58cf5d7297612b2bf00da3a751c
BrianLusina <lusinabrian@gmail.com>
2017-04-10 20:52:52 +0300

    implement secret handshake

--- jssnips/algorithms/secret-handshake/secret-handshake.js
+++ jssnips/algorithms/secret-handshake/secret-handshake.js
@@ -1,9 +1,37 @@
 class SecretHandshake{
+    // perform a sanity check and throw an error early
     constructor(num){
-        this.num = num;
+        if(typeof num !== "number"){
+            throw new Error("Handshake must be a number");
+        }else{
+            this.num = num;
+        }
+        this.allCommands = [ 'wink', 'double blink', 'close your eyes', 'jump', 'REVERSE' ];
     }
 
+    /**
+     * Create a list of commands from the provided number
+     * @returns {Array} String array with the secret handshake
+     */
+    commands(){
+        let resultCommands = [];
 
+        for(var x = 0;x < this.allCommands.length; x++){
+            let currentCommand = this.allCommands[x];
+
+            let hasCommand = (this.num & Math.pow(2, x));
+
+            if(hasCommand){
+                if(currentCommand == "REVERSE"){
+                    resultCommands.reverse();
+                }else{
+                    resultCommands.push(this.allCommands[x]);
+                }
+            }
+        }
+
+        return resultCommands;
+    }
 }
 
 module.exports = SecretHandshake;
