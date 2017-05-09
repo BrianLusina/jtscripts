c9117eae2b3098b74f502ff2f28aefb1597077e8
Brian Lusina <lusinabrian@gmail.com>
2017-05-09 21:44:46 +0300

    Update closed bracket implemtation
    add solution to closed bracket

--- jssnips/string_words/closed_bracket_word/closed-bracket-word.js
+++ jssnips/string_words/closed_bracket_word/closed-bracket-word.js
@@ -1,11 +1,23 @@
+/**
+ * Checks if a word is a closed bracket word, i.e. 
+ * if x is the ith letter of the English alphabet, and y is the (27 - i)th for each valid i (1-based). 
+ * For example, 'z' is the counterpart of 'a' and vice versa, 'y' is the counterpart of 'b', and so on. 
+ * @param{String} word input word
+ * @returns{Boolean} True if word is PCBW, false otherwise
+ */
 function closedBracketWord(word) {
   if(word.length % 2 !== 0){
     return false;
   }
-  
-  for(var x = 0; x < word.length; x++){
-    
+  let count = Math.floor(word.length / 2);
+
+  for(let x = 0; x < count; x++){
+      if(word.charCodeAt(x) - 96 + word.charCodeAt(word.length - x - 1) - 96 !== 27){
+          return false
+      }
   }
+  return true;
 }
+
 
 module.exports = closedBracketWord;
--- tests/string_words/closedbracketword.spec.js
+++ tests/string_words/closedbracketword.spec.js
@@ -1,33 +1,34 @@
 var closedBracketWord = require("../../jssnips/string_words/closed_bracket_word/closed-bracket-word")
+var expect = require('chai').expect;
 
 describe("Basic Tests", function(){ 
     
     it("It should works for basic tests.", function(){
-        expect(closedBracketWord("abiryz")).toEqual(true)
+        expect(closedBracketWord("abiryz")).to.equal(true)
     });
 
     it("", function(){
-        expect(closedBracketWord("aibryz"),false)
+        expect(closedBracketWord("aibryz")).to.equal(false)
     });
 
     it("random word", function(){
-        expect(closedBracketWord("abitryz"),false)
+        expect(closedBracketWord("abitryz")).to.equal(false)
     });
 
     it("some random word", function(){
-        expect(closedBracketWord("zhuazfsa"),true)
+        expect(closedBracketWord("zhuazfsa")).to.equal(true)
     });
 
     it("baby", function(){
-        expect(closedBracketWord("baby"),false)
+        expect(closedBracketWord("baby")).to.equal(false)
     });
 
     it("grit", function(){
-        expect(closedBracketWord("grit"),true)
+        expect(closedBracketWord("grit")).to.equal(true)
     });
 
     it("Foul", function(){
-        expect(closedBracketWord("foul"),false)
+        expect(closedBracketWord("foul")).to.equal(false)
     });
 
 })
