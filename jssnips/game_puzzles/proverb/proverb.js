5a9d9fea4ccb2d598658f49dc985ecec5fa02b4b
Brian Lusina <lusinabrian@gmail.com>
2017-05-10 21:03:00 +0300

    proverb solution

--- jssnips/game_puzzles/proverb/proverb.js
+++ jssnips/game_puzzles/proverb/proverb.js
@@ -1,41 +1,39 @@
-class Proverb{
-    constructor(){
-        this.last = arguments[arguments.length - 1];
-        this.chain = Array.from(arguments);
-        this.options = {};
+module.exports = function () {
+  var last = arguments[arguments.length - 1];
+  var chain = Array.from(arguments);
+  this.options = {};
 
-        if(typeof this.last == "object" && this.last.hasOwnProperty("qualifier")){
-            this.options = this.chain.pop();
-        }
+  if (typeof last == "object" && last.hasOwnProperty("qualifier")) {
+    this.options = chain.pop();
+  }
 
-        this.qualifier = this.options.qualifier ? this.options.qualifier + " " : "";
-    }
+  this.chain = chain;
+  this.qualifier = this.options.qualifier ? this.options.qualifier + ' ' : '';
 
-    /**
-     * 
-     */
-    toString(){
-        return this.eventChain + this.conclustion;
-    }
+  this.toString = function () {
+    return this.chainOfEvents() + this.conclusion();
+  }.bind(this);
 
-    eventChain(){
-        return this.causeAndEffects.map(function(entry){
-            return "For want of a " + entry.cause + " the "  + entry.effect + " was lost.\n";
-        }).join("");
-    }
+  this.chainOfEvents = function () {
+    return this.causesAndEffects().map(function (entry) {
+      return "For want of a " + entry.cause +
+        " the " + entry.effect + " was lost.\n";
+    }.bind(this)).join('');
+  }.bind(this);
 
-    causeAndEffects(){
-        return this.chain.reduce(function(array, event, index){
-            if(index < this.chain.length - 1){
-                array.push({ cause: event, effect: this.chain[index + 1] });
-            }
-            return array;
-        }, []);
-    }
+  this.causesAndEffects = function () {
+    return this.chain.reduce(function (array, event, index) {
+      if (index < this.chain.length - 1) {
+        array.push({
+          cause: event,
+          effect: this.chain[index + 1]
+        });
+      }
+      return array;
+    }.bind(this), []);
+  }.bind(this);
 
-    conclusion(){
-        return "And all for the want of a " + this.qualifier + this.chain[0] + ".";
-    }
-}
-
-module.exports = Proverb;
+  this.conclusion = function () {
+    return "And all for the want of a " + this.qualifier + this.chain[0] + ".";
+  }.bind(this);
+};
