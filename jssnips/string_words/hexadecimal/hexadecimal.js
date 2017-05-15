17c10441c0d05d6dd63ec3d87def5b8db3113d45
Brian Lusina <lusinabrian@gmail.com>
2017-05-15 19:49:44 +0300

    hexadecimal test cases

---
+++ jssnips/string_words/hexadecimal/hexadecimal.js
@@ -0,0 +1,15 @@
+class Hexadecimal{
+    constructor(hexa){
+        this.hexa = hexa;
+    }
+
+    /**
+     * Converts a hexadecimal number to decimal
+     * 
+     */
+    toDecimal(){
+
+    }
+}
+
+module.exports = Hexadecimal;
