5616550267d473300b6cd4afe8a2ff16bb035c0b
Brian Lusina <lusinabrian@gmail.com>
2017-05-17 20:36:02 +0300

    second iteration on lsp, sanity checks

--- jssnips/math_numbers/largestSeriesProduct/largestSeriesProduct.js
+++ jssnips/math_numbers/largestSeriesProduct/largestSeriesProduct.js
@@ -4,6 +4,19 @@ class Series{
     }
 
     /**
+     * Checks the input for validity
+     * @param {Object} input object input
+     */
+    checkInput(input){
+        if(typeof input !== "number" || input < 0){
+            throw Error("Invalid input.");
+        }
+
+        if(input > this.series.length){
+            throw Error("Slice size is too big.");
+        }
+    }
+    /**
      * Gets the largest product from a series made of digits
      * 
      * @param {Number} num digits to calculate the largest product in a series
@@ -12,6 +25,14 @@ class Series{
     largestProduct(num){
         // stores the results of the computation
         let result = {};
+
+        // perform sanity checks
+        this.checkInput(num);
+
+        // if number is 0 and series length is 0
+        if((num === 0 && this.series.length === 0)|| num === 0){
+            return 1;
+        }
 
         // loop through string, incrementing the counter by num
         for(let x = 0;x < this.series.length; x += num){
