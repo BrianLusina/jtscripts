344a11e8ff6c263418475bb9522b0a1cb3ba7b71
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2018-09-17 20:45:28 +0300

    test: fix failing tests
    fix failing tests and add chai package

--- src/cryptography/atbash_cipher/atbashcipher.js
+++ src/cryptography/atbash_cipher/atbashcipher.js
@@ -1,41 +1,42 @@
 "use-strict";
 
 var LETTERS = 'abcdefghijklmnopqrstuvwxyz';
-var REVERSED_LETTERS = LETTERS.spltest("").reverse().join("");
+var REVERSED_LETTERS = LETTERS.split("").reverse().join("");
 
 module.exports = {
 
-    /**Inserts spacing to the words in the phrase after a specific interval.
-     * @param str, the string to insert spacing
-     * @param interval, the interval to insert
-     */
-    insertSpacing(str, interval){
-        // matches new characters except line terminators
-        var matcher = new RegExp(".{1,"+ interval +"}", "g");
-        return str.match(matcher).join(" ");
-    },
+  /**Inserts spacing to the words in the phrase after a specific interval.
+   * @param str, the string to insert spacing
+   * @param interval, the interval to insert
+   */
+  insertSpacing(str, interval) {
+    // matches new characters except line terminators
+    var matcher = new RegExp(".{1," + interval + "}", "g");
+    return str.match(matcher).join(" ");
+  },
 
-    /**
-     * If the char matches a digit, add it to the array,
-     * if not, invert the character
-     * @param character: character to check for 
-     */
-    invert(character){
-        this.push(character.match(/\d/) ? character : LETTERS[REVERSED_LETTERS.indexOf(character)]);
-    },
+  /**
+   * If the char matches a digit, add it to the array,
+   * if not, invert the character
+   * @param character: character to check for 
+   */
+  invert(character) {
+    this.push(character.match(/\d/) ? character : LETTERS[REVERSED_LETTERS.indexOf(character)]);
+  },
 
-    /**
-     * Does the actual encoding,
-     * lowercases the phrase, splits it into an array and for each character checks if it matches
-     * the callback function and adds it to the characters array
-     * Inserts spacing to the phrase at intervals of 5 and returns the encoded phrase.
-     * @param phrase, the phrase to encode, usually a string.
-     * @returns an encoded message
-     */
-    encode(phrase){
-        var encoded = "", characters = [];
-        phrase.toLowerCase().spltest("").forEach(this.invert, characters);
-        encoded = this.insertSpacing(characters.join(""), 5);
-        return encoded;
-    }
+  /**
+   * Does the actual encoding,
+   * lowercases the phrase, splits it into an array and for each character checks if it matches
+   * the callback function and adds it to the characters array
+   * Inserts spacing to the phrase at intervals of 5 and returns the encoded phrase.
+   * @param phrase, the phrase to encode, usually a string.
+   * @returns an encoded message
+   */
+  encode(phrase) {
+    var encoded = "",
+      characters = [];
+    phrase.toLowerCase().spltest("").forEach(this.invert, characters);
+    encoded = this.insertSpacing(characters.join(""), 5);
+    return encoded;
+  }
 }
--- src/math_numbers/SumOfPairs/sumofpairs.spec.js
+++ src/math_numbers/SumOfPairs/sumofpairs.spec.js
@@ -2,14 +2,14 @@ var test = require("ava");
 var expect = require("expect");
 var sum_pairs = require('./sumofpairs.js');
 
-l1 = [1, 4, 8, 7, 3, 15];
-l2 = [1, -2, 3, 0, -6, 1];
-l3 = [20, -13, 40];
-l4 = [1, 2, 3, 4, 1, 0];
-l5 = [10, 5, 2, 3, 7, 5];
-l6 = [4, -2, 3, 3, 4];
-l7 = [0, 2, 0];
-l8 = [5, 9, 13, -3];
+let l1 = [1, 4, 8, 7, 3, 15];
+let l2 = [1, -2, 3, 0, -6, 1];
+let l3 = [20, -13, 40];
+let l4 = [1, 2, 3, 4, 1, 0];
+let l5 = [10, 5, 2, 3, 7, 5];
+let l6 = [4, -2, 3, 3, 4];
+let l7 = [0, 2, 0];
+let l8 = [5, 9, 13, -3];
 
 test("Basic: [" + l1 + "] should return [1, 7] for sum = 8", t => {
   expect(sum_pairs(l1, 8)).toEqual([1, 7]);
