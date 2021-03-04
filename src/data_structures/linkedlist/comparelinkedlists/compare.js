7a99c12e9eb6d6da1ac9cd8bc13db22ad597387a
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-04 21:53:08 +0300

    feat(ds): compare linked lists

---
+++ src/data_structures/linkedlist/comparelinkedlists/compare.js
@@ -0,0 +1,22 @@
+function CompareLists(llist1, llist2) {
+    if(!llist1 || !llist2) {
+        return false
+    }
+    
+    let pointerOne = llist1
+    let pointerTwo = llist2
+    
+    while(pointerOne && pointerTwo) {
+        if(pointerOne.data != pointerTwo.data) {
+            return false
+        }
+        
+        pointerOne = pointerOne.next
+        pointerTwo = pointerTwo.next
+    }
+    
+    if((pointerOne && !pointerTwo) || (pointerTwo && !pointerOne)) {
+        return false
+    }
+    return true
+}
