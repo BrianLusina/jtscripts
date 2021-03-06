6d6f9a1ef7dc496eed7cbc995fd5625728f6e32b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-06 16:16:15 +0300

    feat(algorithms): perm missing element

---
+++ src/algorithms/permMissingElem/permMissingElem.ts
@@ -0,0 +1,15 @@
+/**
+ * Finds the missing element from an array of numbers
+ * @param {Number[]} numbers Array of numbers
+ * @returns {Number}
+ */
+export function permMissingElem(numbers: number[]): number {
+    let missingElement = numbers.length + 1
+
+    for (let index = 0; index < numbers.length; index++) {
+        const value = numbers[index];
+        missingElement = missingElement ^ value ^ (index + 1)
+    }
+
+    return missingElement
+}
