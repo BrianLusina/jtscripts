bdad0c500b73cdded8985c7a9a8772b8a642c1ae
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-11 14:37:22 +0300

    feat(binarytrees): in order traversal

--- src/data_structures/trees/BinarySearchTree.ts
+++ src/data_structures/trees/BinarySearchTree.ts
@@ -16,4 +16,71 @@ export default class BinarySearchTree<T> extends Tree<BinaryTreeNode<T>> {
      push(node: BinaryTreeNode<T>): void {
         throw new Error("Method not implemented.");
     }
+
+    /**
+     * Inorder Traversal recursively
+     * @param {BinaryTreeNode} root 
+     * @returns {any[]} Array of values from each node in the tree
+     */
+    inorderTraversalRecurse(root: BinaryTreeNode<T> | null): any[] {
+        let result = [];
+        if(root) {
+            if(root.left) {
+                this.inorderTraversalRecurse(root.left)
+            }
+
+            result.push(root.data)
+
+            if (root.right) {
+                this.inorderTraversalRecurse(root.right)
+            }
+        }
+
+        return result;
+    }
+
+    inorderTraversalIteratively(): any[] {
+        let result = [];
+        let stack = [];
+        let current = this.root;
+        
+        while(current || stack.length !== 0) {
+            while(current) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop()
+            result.push(current?.data);
+            current = current?.right
+        }
+
+        return result
+    }
+
+    inorderTraversalMorrisTraversal(): any[] {
+        let result = [];
+        let current = this.root;
+        let pre = null;
+
+        while(current) {
+            if(!current.left) {
+                result.push(current.data);
+                current = current.right;
+            } else {
+                pre = current.left;
+
+                while(pre.right){
+                    pre = pre.right;
+                }
+
+                pre.right = current;
+                let temp = current;
+                current = current.left;
+                temp.left = null;
+            }
+        }
+
+        return result;
+    }
 }
--- src/data_structures/trees/Tree.ts
+++ src/data_structures/trees/Tree.ts
@@ -8,4 +8,23 @@ export default abstract class Tree<TreeNode> {
      * @param {TreeNode} node
      */
     abstract push(node: TreeNode): void;
+
+    abstract inorderTraversalRecurse(root: TreeNode | null): any[];
+
+    /**
+     * Uses a stack to get the nodes of a Tree
+     */
+    abstract inorderTraversalIteratively(): any[];
+
+    /**
+     * Step 1: Initialize current as root
+     * Step 2: While current is not NULL,
+     *  If current does not have left child
+     *      a. Add current’s value
+     *      b. Go to the right, i.e., current = current.right
+     *  Else
+     *      a. In current's left subtree, make current the right child of the rightmost node
+     *      b. Go to this left child, i.e., current = current.left
+     */
+    abstract inorderTraversalMorrisTraversal(): any[];
 }
