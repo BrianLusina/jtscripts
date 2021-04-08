2c3a67e258e4952562623908b61b24171ed349e0
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-04-08 09:02:47 +0300

    feat(tsstrings): parse deadfish

---
+++ src/tsstrings/deadfish/parse.ts
@@ -0,0 +1,30 @@
+/** return the output array and ignore all non-op characters */
+export function parse(data: string): number[] {
+  let value = 0;
+  const result: number[] = [];
+
+  for (const d of data) {
+      switch (d) {
+        case "i":
+            value += 1            
+            break;
+        
+        case "s":
+            value *= value
+            break;
+        
+        case "d":
+            value -= 1
+            break;
+        
+        case "o":
+            result.push(value);
+            break;
+                
+        default:
+            break;
+      }     
+  }
+
+  return result;
+}
