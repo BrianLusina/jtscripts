5e184d6bbdff6426d4acaa40d1acf3bc14bb7eb0
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-05-04 10:56:43 +0300

    feat(math): k closest points to origin

---
+++ src/math/kclosest/kclosest.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Sort the points by distance, then take the closest K points.
+ * This sorts the points in-place
+ * @param points 2D array of points on the plane
+ * @param k number of points to return
+ * @returns k closest points to origin
+ */
+function kClosest(points: number[][], k: number): number[][] {
+    points.sort((x, y) => (x[0] ** 2 + x[1] ** 2) -(y[0] ** 2 + y[1] ** 2))
+    return points.slice(0, k)
+};
