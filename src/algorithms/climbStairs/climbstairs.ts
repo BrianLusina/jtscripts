56c2d05f31e887fb1fc4fd62a00fcaee80bb0e44
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-05-19 13:22:02 +0300

    feat(algorithms): climb stairs

---
+++ src/algorithms/climbStairs/climbstairs.ts
@@ -0,0 +1,16 @@
+export function climbStairs(n: number): number {
+    if (n === 1) {
+        return 1;
+    }
+
+    let first = 1;
+    let second = 2;
+
+    for(let x = 3;x <= n; x++) {
+        const third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return second
+}
