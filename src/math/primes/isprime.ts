b7e4fbe6c46e02f55dc7e8d32426f4f4df3aecfc
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-05-21 09:25:49 +0300

    feat(math): steps in primes

--- src/math/primes/isprime.ts
+++ src/math/primes/isprime.ts
@@ -1,6 +1,14 @@
 export default function isPrime(n: number): boolean {
-    for (let i =2; i < n; i ++) {
+    if (n < 2) {
+        return false
+    } else if (n === 2) {
+        return true
+    } else if (n % 2 === 0) {
+        return false
+    }
+
+    for (let i = 3; i <= Math.sqrt(n); i += 2) {
         if(n % i === 0) return false;
     }
-    return n !== 1;
+    return true;
 }
---
+++ src/math/primes/stepsinprimes.ts
@@ -0,0 +1,22 @@
+import isprime from "./isprime";
+
+
+export function step(g: number, m: number, n: number): number[] | null {
+    if ((n - m) < g) {
+        return null;
+    }
+
+    if ((n - m) == g && (isprime(m) && isprime(n))) {
+        return [m, n]
+    }
+
+    for (let x = m; x <= n; x++) {
+        const second = x + g;
+
+        if(isprime(x) && isprime(second)) {
+            return [x, second]
+        }
+    }
+
+    return null;
+}    
