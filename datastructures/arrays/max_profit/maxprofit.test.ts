e897e284b5dfc6bc788b00c615130936e99b4d9e
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-01-18 10:21:48 +0300

    feat(ds): arrays - max profit

---
+++ datastructures/arrays/max_profit/maxprofit.test.ts
@@ -0,0 +1,11 @@
+import maxProfit from "./maxprofit";
+
+describe("maxProfit", () => {
+    it("should return 5 for prices of 7,1,5,3,6,4", () => {
+        expect(maxProfit([7, 1, 5, 3, 6, 4])).toBe(5)
+    })
+
+    it("should return 0 for prices of 7,6,4,3,1", () => {
+        expect(maxProfit([7, 6, 4, 3, 1])).toBe(0)
+    })
+})
---
+++ datastructures/arrays/max_profit/maxprofit.ts
@@ -0,0 +1,13 @@
+export default function maxProfit(prices: number[]): number {
+    let maxProfit = 0
+    let minPrice = Infinity
+
+    for (const price of prices) {
+        if (price < minPrice) {
+            minPrice = price
+        } else if (price - minPrice > maxProfit) {
+            maxProfit = price - minPrice
+        }
+    }
+    return maxProfit
+}
