9cc41e3e0badcc36cd9ef2179c2781e7270744dd
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-02-22 09:03:39 +0300

    feat(puzzles): resistor color

---
+++ puzzles/resistorColor/resistor-color.ts
@@ -0,0 +1,22 @@
+type Colors = typeof COLORS[number]
+
+export const colorCode = (color: Colors): number => {
+  const value = COLORS.indexOf(color)
+  if (value === -1) {
+    throw new Error(`Invalid color: ${color}`)
+  }
+  return value
+}
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
