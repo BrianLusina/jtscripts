021d2101ce33e8f0b2f0c3dafaf7a9c6bfc7139b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-07-14 09:59:58 +0300

    feat(fp): functional add

---
+++ functionalprogramming/funcionaladd/functionaladd.test.ts
@@ -0,0 +1,20 @@
+import add from './functionaladd';
+
+describe("Functional Add", () => {
+    it("should return 4 for add(1)(3)", () => {
+        const actual = add(1)(3)
+        const expected = 4
+        expect(actual).toEqual(expected)
+    })
+
+    for (let i = 0; i < 100; i++) {
+        const num1 = Math.floor(Math.random() * 1001) - 500
+        const num2 = Math.floor(Math.random() * 1001) - 500
+        const actual = add(num1)(num2)
+        const expected = num1 + num2
+
+        it(`should return ${expected} for add(${num1})(${num2})`, () => {
+          expect(actual).toEqual(expected)
+        })
+    }
+})
