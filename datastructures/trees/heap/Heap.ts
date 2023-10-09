7c0d4ff9fa1c2731cc70d995e0784d3926ea547b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-10-09 07:56:48 +0300

    feat(heap): add size method
    Enables retrieving the number of elements in a heap

--- datastructures/trees/heap/Heap.ts
+++ datastructures/trees/heap/Heap.ts
@@ -15,4 +15,10 @@ export default abstract class Heap<T> {
    * @returns {T} root at heap
    */
   abstract delete(): T;
+
+  /**
+   * Returns the size of the heap, i.e. the number of elements in the heap
+   * @returns {number} number of elements in the heap
+   */
+  abstract size(): number;
 }
