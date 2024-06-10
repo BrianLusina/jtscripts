831174a1b13efa3d333aeedb22c9b529701b6ff3
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-06-10 09:35:19 +0300

    refactor(hash-map): update api

--- datastructures/hashmap/HashMap.ts
+++ datastructures/hashmap/HashMap.ts
@@ -1,55 +1,68 @@
-interface Item {
-  key: string | number;
-  value: string | number | unknown;
-}
+import { generateHash } from '../../utils/hash';
 
-type ItemList = Array<Item>;
+type HashItem<K, V> = {
+  key: K;
+  value: V;
+};
 
-export default class HashMap {
-  private size: number;
-  private map: ItemList[];
+export default class HashMap<K, V> {
+  private maxSize: number;
+  private map: Map<string, HashItem<K, V>>;
   // Current MAX_SIZE, is 10**3, change this to change the default size of the hashmap
   private MAX_SIZE = 10 ** 3;
+  private currentSize: number;
 
   constructor(size?: number) {
-    this.size = size || this.MAX_SIZE;
-    this.map = new Array(this.size);
+    this.maxSize = size || this.MAX_SIZE;
+    this.currentSize = 0;
+    this.map = new Map<string, HashItem<K, V>>();
   }
 
-  private hash(key: number): number {
-    return key % this.size;
+  private hash(key: K): string {
+    return generateHash(key);
   }
 
-  put(key: number, value: number): void {
-    const hash = this.hash(key);
-
-    if (!this.map[hash]) {
-      this.map[hash] = [];
+  put(key: K, value: V): void {
+    if (this.isFull()) {
+      throw new Error(`HashMap is currently full. Current capacity is ${this.currentSize}`);
     }
 
-    const itemList = this.map[hash];
-    const item = itemList.find((item) => item.key === key);
+    const hash = this.hash(key);
+    const newItem = { key: key, value: value };
+    const item = this.map.get(hash);
+
     if (item) {
       item.value = value;
+      this.map.set(hash, item);
     } else {
-      itemList.push({ key, value });
+      this.map.set(hash, newItem);
     }
+
+    this.currentSize += 1;
   }
 
-  get(key: number): number | string | undefined | unknown {
-    const hashed = this.hash(key);
-    const itemlist = this.map[hashed] || [];
-    const item = itemlist.find((item) => item.key === key);
+  get(key: K): V | undefined {
+    const hash = this.hash(key);
+    const item = this.map.get(hash);
     return item ? item.value : undefined;
   }
 
-  remove(key: number): void {
-    const hashed = this.hash(key);
+  remove(key: K): void {
+    const hash = this.hash(key);
 
-    if (!this.map[hashed]) {
+    if (!this.map.has(hash)) {
       return;
     }
 
-    this.map[hashed] = this.map[hashed].filter((item) => item.key !== key);
+    this.map.delete(hash);
+    this.currentSize -= 1;
+  }
+
+  isFull(): boolean {
+    return this.maxSize === this.currentSize;
+  }
+
+  public get capacity(): number {
+    return this.currentSize;
   }
 }
--- datastructures/hashmap/README.md
+++ datastructures/hashmap/README.md
@@ -2,16 +2,14 @@
 
 Design a HashMap without using any built-in hash table libraries.
 
-Implement the MyHashMap class:
+Implement the HashMap class:
 
-MyHashMap() initializes the object with an empty map.
+HashMap() initializes the object with an empty map.
 void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
 int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
 void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
 
 Constraints and assumptions
-For simplicity, are the keys integers only?
-Yes
 For collision resolution, can we use chaining?
 Yes
 Do we have to worry about load factors?
